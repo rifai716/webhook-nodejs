dbe7dc4dc38bb60bd2c6267a5dfe35aaa2041267
Ahmad Rifa'i <ahmadrifai716@gmail.com>
2019-09-03 17:32:08 +0700

    fixing 1

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.post('/', (req, res) => {
      const chatId = req.body.message.chat.id;
      const sentMessage = req.body.message.text;
      // Regex for hello
-     if (sentMessage.match(/hello/gi)) {
+     if (sentMessage != '') {
           axios.post(`${url}${apiToken}/sendMessage`,
                {
                     chat_id: chatId,
